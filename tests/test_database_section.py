92dc25e7205a8a551e00ffd58b3debbb6f38d027
GrigoryTishkov <grisatiskov@gmail.com>
2025-05-10 11:32:54 +0300

    Update Tests

---
+++ tests/test_database_section.py
@@ -0,0 +1,29 @@
+import pytest
+from framework.config_reader import ConfigReader
+from framework.validators import ConfigValidator
+
+@pytest.fixture()
+def config():
+    reader = ConfigReader()
+    return reader.read_config()
+
+def test_database_section_exists(config):
+    assert 'Database' in config.sections()
+
+@pytest.mark.parametrize("param,validator,args",[
+                        ("Host", "string_validator", None),
+                        ("Port", "integer_validator", (1, 65535)),
+                        ("User", "string_validator", None),
+                        ("Password", "string_validator", None),
+                        ("Database", "database_validator", None),
+                        ("Name", "string_validator", None),
+                        ("PoolSize", "integer_validator", (1, 20))
+])
+def test_database_section_parameters(config, param, validator, args):
+    assert param in config['Database'], f'Параметр {param} отсутствует в секции [Database]'
+    value = config['Database'][param]
+    validator_func = getattr(ConfigValidator, validator)
+    if args:
+        assert validator_func(value, *args), f'Тест упал на {param}={value}'
+    else:
+        assert validator_func(value), f'Тест упал на {param}={value}'
